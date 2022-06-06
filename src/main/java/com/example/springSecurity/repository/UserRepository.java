1b710e235d218d94b69a94d961fff9217b18755d
pradeepm-m <pradeep@moneynmore.com>
2022-06-06 15:46:07 +0530

    First Commit

---
+++ src/main/java/com/example/springSecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.springSecurity.repository;
+
+import com.example.springSecurity.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
